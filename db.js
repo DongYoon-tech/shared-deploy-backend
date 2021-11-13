4bc062a8d3eb8b82538944c8c88d9ca8a848b816
Dongyuni <nunsaram26@gmail.com>
2021-11-12 20:21:45 -0800

    bug found on the db changed from const to let

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 const { Client } = require("pg");
 const { DB_URI } = require("./config");
 
+let db;
 if (process.env.NODE_ENV === "production") {
-    const db = new Client({
+    db = new Client({
         connectionString: DB_URI,
         ssl: {
             rejectUnauthorized: false
